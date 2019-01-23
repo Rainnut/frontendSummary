25fb475081c39ec82a1e64f02a5e0f8cc56b6499
dongyang02.zhang <dongyang02.zhang@pjbest.com>
2019-01-23 11:07:53 +0800

    提交js基础

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # frontendSummary
 前端技术点整理
+
+# 拉取代码失败报错：1080
+git config --global http.proxy
+git config --global --unset http.proxy
---
+++ es/basis.js
@@ -0,0 +1,76 @@
+ECMAScript 3.1发布 后来改名 ECMAScript 5
+ECMAScript 和 JavaScript的关系？
+前者是后者的规范，后者是前者的一种实现
+
+let、const 
+1、声明的变量值再所在的代码块内有效，不存在变量提升。
+2、不可重复定义
+3、const声明的值和对象地址不可改变。
+
+-----------作用域----------------
+js中的作用域是以函数为边界
+function foo(){
+	var x = 1;
+	if(x){
+		var x = 2;
+	}
+	console.log(x);  // 2
+}
+function foo(){
+	var x = 1;
+	if(x){
+		(function(){
+			var x = 2;
+		}());
+	}
+	console.log(x);  // 1
+}
+function foo(){
+	var x = 1;
+	if(x){
+		(function(){
+			x = 2;
+		}());
+	}
+	console.log(x);  // 2
+}
+
+
+暂时性死区，只要块级作用域内存在let命令，它所声明的变量就“绑定”这个区域，不再受外部的影响。使用let命令声明变量之前，该变量都是不可用的。
+var tmp = 123;
+if(true){
+	tmp = 'abc';  //referenceError
+	let tmp;
+}
+
+-----------解构赋值--------------
+let [head, ... tail] = [1,2,3,4];
+head // 1
+tail // [2,3,4]
+
+let [a,b,...z] = ['1'];
+a // '1'
+b // undefined
+z // []
+
+let { foo : baz } = {foo:'123',bar:'222'};
+baz // '123'
+
+let {x:y = 3} = {}
+y  // 3
+
+const [a,b,c] = 'hello';
+a //'h'
+b //'e'
+c //'l'
+
+[[1,2],[3,4]].map(([a,b])=> a+ b )  // [3,7]
+
+-----------Map对象--------------
+let map = new Map();
+map.set('first','hello');
+map.set('second','hi');
+for(let [key,value] of map){}
+for(let [key] of map){}
+for(let [,value] of map){}
+	
